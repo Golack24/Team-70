59bb86a3db70ee7c0d256d489f10600b14755369
Anas <kivoramanagement@gmail.com>
2025-10-24 13:56:08 +0100

    navbar creation

---
+++ frontend/src/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './navbar.css'; 
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <h1>Navbar</h1>
+                
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
