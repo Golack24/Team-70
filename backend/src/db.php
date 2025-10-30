f6069af20db220b226312ba6b81bffb3156b7b23
Sebastian Lupascu <Golack90@gmail.com>
2025-10-30 12:03:11 +0000

    established db setup
    Added try and catch with the database connection settings ( waiting for aston so we can use their server)

---
+++ backend/src/db.php
@@ -0,0 +1,30 @@
+<?php
+
+//Database Connection Settings
+$host = 'localhost';
+$dbname = ''; // aston dbname
+$user= ''; // Aston user
+$pass = ''; //Aston pass
+
+
+
+try {
+    $pdo = new PDO(); // waiting for the db to be established by aston
+
+    // PDO options for error handling
+    $pdo-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    
+} catch    (PDOException $e) {
+    http_response_code(500);
+    echo json_encode ({         // if connections fails it will stop the script and show the error message down below
+        "error" => "database connection failed"
+        "details" => $e->getMessage()
+    });
+    exit;
+}
+exit;
+
+
+
+?>
