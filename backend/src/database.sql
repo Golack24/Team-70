4c8825844e3fc2f88045debbbc2f2a1c13528dc0
Sebastian Lupascu <Golack90@gmail.com>
2025-11-25 17:23:00 +0000

    fixed the db and the sql

--- backend/src/database.sql
+++ backend/src/database.sql
@@ -1,56 +1,78 @@
 CREATE DATABASE IF NOT EXISTS metricdb;
 USE metricdb;
 
+CREATE TABLE users (
+    id int AUTO_INCREMENT PRIMARY key,
+    username VARCHAR(100) DEFAULT NULL,
+    first_name VARCHAR(100) DEFAULT NULL,
+    last_name VARCHAR(100) DEFAULT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(60) NOT NULL,
+    phone VARCHAR(30) DEFAULT NULL,
+    role ENUM('customer','admin','staff') NOT NULL DEFAULT 'customer'
+);
+
+CREATE TABLE categories (
+    id int AUTO_INCREMENT PRIMARY key,
+    name VARCHAR(255),
+    slug VARCHAR(100) NOT NULL UNIQUE,
+);
 
 CREATE TABLE products (
-id int  AUTO_INCREMENT PRIMARY key 
-name VARCHAR(255) NOT NULL,
-price decimal(10,2) NOT NULL,
-stock INT default 0,
-description TEXT,
-image VARCHAR(255) DEFAULT NULL,
-category_id INT,    
-FOREIGN Key (category_id) REFERENCES categories(id) ON Delete SET NULL 
+    id int  AUTO_INCREMENT PRIMARY key,
+    name VARCHAR(255) NOT NULL,
+    price decimal(10,2) NOT NULL,
+    stock INT default 0,
+    description TEXT,
+    image VARCHAR(255) DEFAULT NULL,
+    category_id INT,    
+    FOREIGN Key (category_id) REFERENCES categories(id) ON Delete SET NULL 
 
 );
 
 CREATE TABLE reviews (
-id int AUTO_INCREMENT PRIMARY key,
-users_id INT NOT NULL,
-product_id INT NOT NULL,
-rating TINYINT  NOT NULL CHECK (rating BETWEEN 1 AND 5),
-comment TEXT,
-FOREIGN Key (user_id) REFERENCES users(id) ON Delete CASCADE,
-FOREIGN Key (product_id) REFERENCES products(id) ON Delete CASCADE
+    id int AUTO_INCREMENT PRIMARY key,
+    users_id INT NOT NULL,
+    product_id INT NOT NULL,
+    rating TINYINT  NOT NULL CHECK (rating BETWEEN 1 AND 5),
+    comment TEXT,
+    FOREIGN Key (user_id) REFERENCES users(id) ON Delete CASCADE,
+    FOREIGN Key (product_id) REFERENCES products(id) ON Delete CASCADE
 );
 
-CREATE TABLE categories (
-id int AUTO_INCREMENT PRIMARY key,
-name VARCHAR(255),
-slug VARCHAR(100) NOT NULL UNIQUE,
-);
+
 
 CREATE TABLE favourites (
-id int AUTO_INCREMENT PRIMARY Key,
-users_id INT NOT NULL,
-product_id INT NOT NULL,
-FOREIGN Key (user_id) REFERENCES users(id) ON Delete CASCADE,
-FOREIGN Key (product_id) REFERENCES products(id) ON Delete CASCADE,
-UNIQUE Key unique_favourite (user_id, product_id)
+    id int AUTO_INCREMENT PRIMARY Key,
+    users_id INT NOT NULL,
+    product_id INT NOT NULL,
+    FOREIGN Key (user_id) REFERENCES users(id) ON Delete CASCADE,
+    FOREIGN Key (product_id) REFERENCES products(id) ON Delete CASCADE,
+    UNIQUE Key unique_favourite (user_id, product_id)
 
 );
 
 CREATE TABLE coupons (
-id int AUTO_INCREMENT PRIMARY Key,
-CODE VARCHAR(25) NOT NULL UNIQUE,
-description VARCHAR(255),
-discount_type ENUM('percentage', 'fixed') NOT NULL,
-discount_value DECIMAL(10,2),
-min_order_value DECIMAL (10,2) DEFAULT 0.00,
-usage_limit INT DEFAULT NULL,
-times_used INT DEFAULT 0,
-is_active TINYINT(1) DEFAULT 1,
-)
+    id int AUTO_INCREMENT PRIMARY Key,
+    CODE VARCHAR(25) NOT NULL UNIQUE,
+    description VARCHAR(255),
+    discount_type ENUM('percentage', 'fixed') NOT NULL,
+    discount_value DECIMAL(10,2),
+    min_order_value DECIMAL (10,2) DEFAULT 0.00,
+    usage_limit INT DEFAULT NULL,
+    times_used INT DEFAULT 0,
+    is_active TINYINT(1) DEFAULT 1,
+);
+
+
+CREATE TABLE orders (
+    id AUTO_INCREMENT PRIMARY Key,
+    users_id INT NOT NULL,
+    total DECIMAL(0,2) NOT NULL DEFAULT 0.00,
+    status ENUM('pending', 'paid', 'shipped', 'delivered', 'cancelled') DEFAULT 'pending',
+    FOREIGN Key (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
 
 CREATE TABLE basket (
     id int AUTO_INCREMENT PRIMARY key,
@@ -60,19 +82,19 @@ CREATE TABLE basket (
     FOREIGN Key (user_id) REFERENCES users(id) ON Delete CASCADE,
     FOREIGN Key (product_id) REFERENCES products(id) ON Delete CASCADE,
     UNIQUE KEY unique_basket_item (user_id, product_id)
-)
+);
 
 CREATE TABLE addresses (
     id int AUTO_INCREMENT PRIMARY key,
     user_id INT NOT NULL,
     line1 VARCHAR(255) NOT NULL,
-    line2 VARCHAR(255),
+    line2 VARCHAR(25VARCHAR(255) DEFAULT NULL,
     city VARCHAR(100) NOT NULL,
     postcode VARCHAR(20) NOT NULL,
     countryVARCHAR(100) NOT NULL,
     is_default TINYINT(1) DEFAULT 0,
     FOREIGN Key (user_id) REFERENCES users(id) ON Delete CASCADE
-)
+);
 
 CREATE TABLE shipping (
     id int AUTO_INCREMENT PRIMARY key,
@@ -82,4 +104,6 @@ CREATE TABLE shipping (
     status ENUM('pending', 'shipped', 'delivered', 'cancelled') DEFAULT 'pending',
     estimated_delivery DATE,
     FOREIGN Key (order_id) REFERENCES orders(id) ON Delete CASCADE
-)
+);
+
+
--- backend/src/db.php
+++ backend/src/db.php
@@ -1,30 +1,29 @@
 <?php
 
-//Database Connection Settings
-$host = 'localhost';
-$dbname = ''; // aston dbname
-$user= ''; // Aston user
-$pass = ''; //Aston pass
-
-
+// Database connection (aston)
+$host = 'localhost'; 
+$dbname = 'cs2team70_db'; 
+$user = 'cs2team70'; 
+$pass = 'co0dJ2qvmGYs7J9SrkVOjXrdQ'; 
 
 try {
-    $pdo = new PDO(); // waiting for the db to be established by aston
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 
-    // PDO options for error handling
-    $pdo-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    
-} catch    (PDOException $e) {
+    //PDO connection
+    $pdo = new PDO($dsn, $user, $pass);
+
+    // PDO options for error handling & fetching
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
     http_response_code(500);
-    echo json_encode ({         // if connections fails it will stop the script and show the error message down below
-        "error" => "database connection failed"
+    echo json_encode([
+        "error" => "Database connection failed",
         "details" => $e->getMessage()
-    });
+    ]);
+
     exit;
 }
-
-
-
 
 ?>
