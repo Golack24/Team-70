e8511f04ad4079f812e37072c7adfed701bb5464
ByteForge124 <eesahk124@gmail.com>
2025-12-04 20:15:03 +0000

    update navbar.jsx

--- frontend/src/navbar.jsx
+++ frontend/src/navbar.jsx
@@ -28,58 +28,68 @@ export default function Navbar({ onNavigate }) {
 
   const mobileLinks = [...leftLinks, ...rightLinks];
 
+  const navClass = menuOpen ? "navbar is-open" : "navbar";
+
   return (
-    <nav className="navbar">
-      <ul className="nav-links">
-        {leftLinks.map((item) => (
-          <li key={item.label}>
-            <a href="#" onClick={handleNav(item.page)}>
-              {item.label}
+    <nav className={navClass}>
+      <div className="navbar-inner">
+        <button
+          type="button"
+          className="nav-toggle"
+          aria-expanded={menuOpen}
+          aria-controls="nav-mobile-menu"
+          onClick={() => setMenuOpen((v) => !v)}
+          aria-label={menuOpen ? "Close menu" : "Open menu"}
+        >
+          <span className="sr-only">{menuOpen ? "Close" : "Menu"}</span>
+          <span className={`nav-toggle-icon ${menuOpen ? "open" : ""}`}>
+            <span />
+            <span />
+            <span />
+          </span>
+        </button>
+
+        <ul className="nav-links">
+          {leftLinks.map((item) => (
+            <li key={item.label}>
+              <a href="#" onClick={handleNav(item.page)}>
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo-img" />
+        </div>
+
+        <div className="nav-spacer" aria-hidden="true" />
+
+        <ul className="nav-links nav-links-right">
+          {rightLinks.map((item) => (
+            <li key={item.label}>
+              <a href="#" onClick={handleNav(item.page)}>
+                {item.label}
+              </a>
+            </li>
+          ))}
+          
+          <li>
+            <a href="#" aria-label="Cart" className="nav-icon-link"onClick={handleNav("checkout")}>
+              <img src={cartIcon} alt="Cart" className="nav-icon-img" />
             </a>
           </li>
-        ))}
-      </ul>
 
-      {/* Centered logo */}
-      <div className="logo-container">
-        <img src={logo} alt="Logo" className="logo-img" />
+          <li className="nav-icon-search">
+            <a href="#" aria-label="Search" className="nav-icon-link">
+              <img src={searchIcon} alt="Search" className="nav-icon-img" />
+            </a>
+          </li>
+        </ul>
       </div>
 
-      {/* Right side links */}
-      <ul className="nav-links nav-links-right">
-        {rightLinks.map((item) => (
-          <li key={item.label}>
-            <a href="#" onClick={handleNav(item.page)}>
-              {item.label}
-            </a>
-          </li>
-        ))}
-        
-        <li>
-          <a href="#" aria-label="Cart" className="nav-icon-link"onClick={handleNav("checkout")}>
-            <img src={cartIcon} alt="Cart" className="nav-icon-img" />
-          </a>
-        </li>
-
-        <li className="nav-icon-search">
-          <a href="#" aria-label="Search" className="nav-icon-link">
-            <img src={searchIcon} alt="Search" className="nav-icon-img" />
-          </a>
-        </li>
-      </ul>
-
-      {/* Mobile toggle */}
-      <button
-        className="nav-toggle"
-        onClick={() => setMenuOpen((v) => !v)}
-        aria-label={menuOpen ? "Close menu" : "Open menu"}
-      >
-        {menuOpen ? "✕" : "☰"}
-      </button>
-
-      {/* Mobile dropdown */}
       {menuOpen && (
-        <div className="nav-mobile-menu">
+        <div className="nav-mobile-menu" id="nav-mobile-menu">
           {mobileLinks.map((item) => (
             <button
               key={item.label}
